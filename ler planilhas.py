7b43ec248d3ecf68ff412a70388130a482289c84
dangm00 <barcelonagl2@gmail.com>
2025-12-02 18:22:31 -0300

    att

--- ler planilhas.py
+++ ler planilhas.py
@@ -1,1 +1,22 @@
+import pandas as pd
+
+# Caminho da planilha
+caminho = r"C:\Users\Dan\Documents\projetinho\Planilha financeira DANIEL.xlsx"
+
+# Ler a aba GERAL
+df = pd.read_excel(caminho, sheet_name="GERAL")
+
+# Substituir NaN em SERVIÇO por "NÃO TRABALHADO"
+df["SERVIÇO"] = df["SERVIÇO"].fillna("NÃO TRABALHADO")
+
+# Substituir NaN em REGIAO por "BAIXADA"
+df["REGIAO"] = df["REGIAO"].fillna("BAIXADA")
+
+# Mostrar tudo
+
+
+print(df)
+
+
+
 
